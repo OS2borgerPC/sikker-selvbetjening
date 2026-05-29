790706e19fc9ac4143594c96304e14c65733d3f7
Agnete Moos <agms@sonderborg.dk>
2026-05-29 14:05:40 +0200

    Add overlay playbooks installation script

---
+++ features/overlay-playbooks/build_files/25-overlay-playbooks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Install overlay entrypoint used by downstream config/image builds.
+install -Dm0755 \
+	/ctx/system_files/usr/libexec/sikker-create-overlay \
+	/usr/libexec/sikker-create-overlay
+
+# Install overlay playbook tree consumed by sikker-create-overlay.
+install -d /usr/libexec/sikker-overlay-playbooks
+cp -a /ctx/system_files/usr/libexec/sikker-overlay-playbooks/. /usr/libexec/sikker-overlay-playbooks/

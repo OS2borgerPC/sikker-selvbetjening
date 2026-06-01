b3f3966ce157f8199463e2685284020c25a24c3f
Agnete Moos <agms@sonderborg.dk>
2026-06-01 14:47:42 +0200

    Fix paths

--- features/sikker-overlay-engine/build_files/25-overlay-playbooks.sh
+++ features/sikker-overlay-engine/build_files/25-overlay-playbooks.sh
@@ -4,9 +4,9 @@ set -ouex pipefail
 
 # Install overlay entrypoint used by downstream config/image builds.
 install -Dm0755 \
-	/tmp/features/overlay-playbooks/system_files/usr/libexec/sikker-create-overlay \
+	/tmp/features/sikker-overlay-engine/system_files/usr/libexec/sikker-create-overlay \
 	/usr/libexec/sikker-create-overlay
 
 # Install overlay playbook tree consumed by sikker-create-overlay.
-install -d /usr/libexec/sikker-overlay-playbooks
-cp -a /tmp/features/overlay-playbooks/system_files/usr/libexec/sikker-overlay-playbooks/. /usr/libexec/sikker-overlay-playbooks/
+install -d /usr/libexec/sikker-overlay
+cp -a /tmp/features/sikker-overlay-engine/system_files/usr/libexec/sikker-overlay/. /usr/libexec/sikker-overlay/

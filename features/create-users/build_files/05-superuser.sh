19e52e29afac9a1fd101cdbfc811a90da29e4925
LimeSlime11 <bartoszbrozio@gmail.com>
2026-06-11 10:35:46 +0200

    add user reset on boot, and centralise name variable

--- features/create-users/build_files/05-superuser.sh
+++ features/create-users/build_files/05-superuser.sh
@@ -2,8 +2,9 @@
 
 set -ouex pipefail
 
-# Create user 1000 to prevent first-boot user creation dialog
-# This user will be the default interactive user
+# this is the default admin user, created once during image build. 
+# used in development
+
 useradd -u 1000 -m -s /bin/bash -c "Super User" superuser
 
 # Set password
@@ -12,5 +13,3 @@ echo "superuser:superuser" | chpasswd
 # Allow sudo with password required
 echo "superuser ALL=(ALL) ALL" >> /etc/sudoers.d/superuser
 chmod 0440 /etc/sudoers.d/superuser
-
-# citizen user will be created on every boot by /usr/libexec/sikker-reset-bruger-home

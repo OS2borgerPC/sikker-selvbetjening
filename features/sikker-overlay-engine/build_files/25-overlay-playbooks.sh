fb9f53d2556284606408f1a33c9f87dc7d1b24ff
Agnete Moos <agms@sonderborg.dk>
2026-07-05 14:12:38 +0200

    Remove installation of post-steps entrypoint

--- features/sikker-overlay-engine/build_files/25-overlay-playbooks.sh
+++ features/sikker-overlay-engine/build_files/25-overlay-playbooks.sh
@@ -7,11 +7,6 @@ install -Dm0755 \
 	/tmp/features/sikker-overlay-engine/system_files/usr/libexec/sikker-build-image \
 	/usr/libexec/sikker-build-image
 
-# Install post-steps entrypoint used by downstream config/image builds.
-install -Dm0755 \
-	/tmp/features/sikker-overlay-engine/system_files/usr/libexec/sikker-run-post-steps \
-	/usr/libexec/sikker-run-post-steps
-
 # Install overlay playbook tree consumed by sikker-build-image.
 install -d /usr/libexec/sikker-overlay
 cp -a /tmp/features/sikker-overlay-engine/system_files/usr/libexec/sikker-overlay/. /usr/libexec/sikker-overlay/
